0ce2dffeaa6c2b281fc5026200f81d12b0b32556
akrasia6858 <mext1234z@gmail.com>
2026-07-25 22:29:23 +0900

    Modified class init function to generate brownian motion. Modified class member time

--- src/sde/bm.py
+++ src/sde/bm.py
@@ -9,12 +9,12 @@ class bm:
         self.nop = nop
         self.ps = np.zeros((self.nop,self.nos+1))
         self.dps = np.zeros((self.nop,self.nos+1))
-    def generate_brownian_motion(self):
         for path_num in range(self.nop):
             self.dps[path_num,0] = 0
             for step_num in range(self.nos):
                 x = np.random.uniform(0, 1)
                 y = np.random.uniform(0, 1)
-                dB = math.sqrt( -2 * math.log(x) ) * math.cos( 2 * math.pi * y )
+                dB = math.sqrt( -2 * math.log(x) * ( self.eot/self.nos ) ) * math.cos( 2 * math.pi * y )
                 self.dps[path_num,step_num+1] = dB
                 self.ps[path_num,step_num+1] = self.ps[path_num,step_num] + dB
+        
