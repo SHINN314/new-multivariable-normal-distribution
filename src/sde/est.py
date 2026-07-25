702e5834ee13c5018cc3a320d90e2f31d1e026af
t5ugu <tsugu0530@outlook.jp>
2026-07-25 23:59:22 +0900

    fix: sentinel of s_div

--- src/sde/est.py
+++ src/sde/est.py
@@ -8,6 +8,7 @@ def est(ps: np.ndarray, time: list[float], t_div: list[float], s_div: list[float
     assert len(time) == noS
 
     ntd = len(t_div)
+    s_div = [-np.inf] + s_div + [np.inf]
     nsd = len(s_div)
     assert ntd < noS
 
@@ -50,3 +51,7 @@ def est(ps: np.ndarray, time: list[float], t_div: list[float], s_div: list[float
         va[:, td] = va[:, td] - mu[:, td] * mu[:, td] * dt
 
     return mu, np.sqrt(va)
+
+if __name__ == '__main__':
+    print(np.zeros((0, 10)))
+    print(bisect_left([], 0))
