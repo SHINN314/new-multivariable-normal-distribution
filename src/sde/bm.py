77d50ad7a2d0dac5abc8519c9604ae9263a52041
akrasia6858 <mext1234z@gmail.com>
2026-07-25 22:10:20 +0900

    Added class method that generates brownian motion values

--- src/sde/bm.py
+++ src/sde/bm.py
@@ -1,10 +1,20 @@
 import numpy as np
+import math
 
 class bm:
-    def __init__(self):
-        self.eot = 1.0
-        self.nos = 10
-        self.time = float(self.nos)
-        self.nop = 1
-        self.ps = np.zeros(self.nop,self.nos)
-        self.dps = np.zeros(self.nop,self.nos)
+    def __init__(self, eot=1.0, nos=10, nop=1):
+        self.eot = eot
+        self.nos = nos
+        self.time = np.linspace(0, 1, 11)
+        self.nop = nop
+        self.ps = np.zeros((self.nop,self.nos+1))
+        self.dps = np.zeros((self.nop,self.nos+1))
+    def generate_brownian_motion(self):
+        for path_num in range(self.nop):
+            self.dps[path_num,0] = 0
+            for step_num in range(self.nos):
+                x = np.random.uniform(0, 1)
+                y = np.random.uniform(0, 1)
+                dB = math.sqrt( -2 * math.log(x) ) * math.cos( 2 * math.pi * y )
+                self.dps[path_num,step_num+1] = dB
+                self.ps[path_num,step_num+1] = self.ps[path_num,step_num] + dB
