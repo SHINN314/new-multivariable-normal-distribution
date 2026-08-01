6fe36843137ee25dc7ff73cd0d7805e8e1b8bb75
SHINN <shinnosukekimura8@gmail.com>
2026-08-01 22:03:04 +0900

    add: 各パス，平均パス，分散の範囲を表示

--- src/sde/plot.py
+++ src/sde/plot.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from bm import bm
+from environment import FIG_ROOT
 
-def plot_ps(ps: np.ndarray, time: float, filename: str | None) -> list[float, float]:
+def plot_ps(ps: np.ndarray, time: float, filename: str | None) -> list[np.ndarray, np.ndarray]:
     """Plot sample paths, mean and std path.
     Show or save the graph.
     Make sure to label the conditions in the graph.
@@ -22,16 +22,18 @@ def plot_ps(ps: np.ndarray, time: float, filename: str | None) -> list[float, fl
 
     time_steps = np.linspace(0, time, ps.shape[1])
 
-    plt.plot(time_steps, ps.T)
+    plt.plot(time_steps, ps.T, color='lightgray', alpha=0.5, label='Sample Paths') # plot all sample paths
+    plt.plot(time_steps, mean_path, color='black', label='Mean Path') # plot mean path
+    plt.fill_between(time_steps, mean_path - std_path, mean_path + std_path, color='blue', alpha=0.2, label='Std Path') # plot std path
     plt.title(f'Sample Paths of Brownian Motion (t={time})')
     plt.xlabel('Time')
     plt.ylabel('Position')
-    plt.legend()
     
     if filename is None:
         plt.show()
     else:
-        plt.savefig(filename)
+        plt.savefig(FIG_ROOT / filename)
+        plt.clf()
         plt.close()
 
     return [mean_path, std_path]
